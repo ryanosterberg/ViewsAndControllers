527bf9f9572edf6e66b003adf58455fb50f0cf80
Mr O <ryan.p.osterberg@gmail.com>
2020-06-21 14:44:45 -0500

    First 4 activities

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/DatesActivityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class DatesActivityController : Controller
+    {
+        // GET: DatesActivity
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: DateTime.Now);
+        }
+        [HttpPost]
+        public ActionResult DateMagic(DateTime inputBirthday)
+        {
+            return View(viewName: "Index", model: inputBirthday);
+        }
+
+
+    }
+}
